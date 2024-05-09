8f1e6fdd4a0244616853339ec19d92e6f4770d4e
Jhennbsit3a <solisjhennyylynpangilinan@gmail.com>
2024-05-09 11:54:02 +0800

    Added my project

---
+++ pages/dashboard.vue
@@ -0,0 +1,78 @@
+<template>
+    <v-card class="mt-4">
+        <v-card-title>
+            <h1>Dashboard</h1>
+            <v-spacer />
+            <v-col md="4">
+                <v-text-field
+                    v-model="search"
+                    append-icon="mdi-magnify"
+                    label="Search"
+                    single-line
+                    hide-details   
+                    >
+                </v-text-field>
+            </v-col>
+        </v-card-title>
+        <v-data-table :headers="headers" :items="students" :search="search">
+            <template slot="item.action" slot-scope="{  }">
+                <v-btn class="primary">EDIT</v-btn>
+                <v-btn class="error">DELETE</v-btn>
+            </template>        
+        </v-data-table>
+    </v-card>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            search: '',
+            headers: [
+                { text:'STUDENT ID', align: 'start', filterable: false, value:'studentID'},
+                { text:'LASTNAME', value:'lastName'},
+                { text:'FIRSTNAME', value:'firstName'},
+                { text:'MIDDLENAME', value:'middleName'},
+                { text:'COURSE', value:'course'},
+                { text:'SECTION', value:'section'},
+                { text:'', value:'action'},
+            ],
+            students: [
+                {
+                    studentID: '21-00194',
+                    lastName: 'SOLIS',
+                    firstName: 'JENNYLYN',
+                    middleName: 'PANGILINAN',
+                    course: 'BSIT',
+                    section: '3A',
+                },
+                {
+                    studentID: '22-00009',
+                    lastName: 'ESGUERRA',
+                    firstName: 'JERDY',
+                    middleName: 'BLOOMFIELD',
+                    course: 'BSIT',
+                    section: '3A',
+                },
+                {
+                    studentID: '21-00710',
+                    lastName: 'NOGOY',
+                    firstName: 'MIKE',
+                    middleName: 'SAMIA',
+                    course: 'BSIT',
+                    section: '3A',
+                },
+                {
+                    studentID: '20-00603',
+                    lastName: 'PINEDA',
+                    firstName: 'GHIO',
+                    middleName: 'MANALILI',
+                    course: 'BSIT',
+                    section: '3A',
+                }
+                
+            ]
+        }
+    },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <div class="d-flex align-center justify-center" style="height: 100vh">
+      <v-card width="400">
+        <div align="center">
+          <v-img
+           src="https://png.pngtree.com/template/20210320/ourmid/pngtree-real-estate-building-logo-image_501124.jpg"
+          width="200"
+          class="mt-4"
+          >
+          </v-img>
+        </div>
+        <div class="mx-6">
+          <v-card-text>
+            <v-row>
+              <v-col cols="6" md="6">
+                <v-btn block outlined>
+                  <v-icon color="red"> mdi-google </v-icon>
+                  google
+                </v-btn>
+              </v-col>
+
+              <v-col cols="6" md="6">
+                <v-btn block outlined>
+                  <v-icon color="primary"> mdi-facebook </v-icon>
+                  facebbok
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-card-text>
+
+          <v-card-text>
+            <v-form ref="form" lazy-validation v-model="valid">
+              <v-text-field
+                outlined
+                label="Email"
+                prepend-inner-icon="mdi-email"
+                :rules="emailRules"
+              >
+              </v-text-field>
+
+              <v-text-field
+                outlined
+                label="Password"
+                prepend-inner-icon="mdi-lock"
+                :rules="passwordRules"
+                type="password"
+              >
+              </v-text-field>
+              <v-btn block color="primary" @click="signin"> Login</v-btn>
+            </v-form>
+          </v-card-text>
+        </div>
+      </v-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout:"auth",
+  data: () => ({
+    valid: true,
+    name: "",
+    passwordRules: [(v) => !!v || "Password is required"],
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    ],
+    select: null,
+    items: ["Item 1", "Item 2", "Item 3", "Item 4"],
+    checkbox: false,
+  }),
+
+  methods: {
+    signin() {
+      if (this.$refs.form.validate){
+        console.log("successfully Login");
+        this.$router.push("/Dashboard");
+      }
+    }
+    }
+      }
+    
+</script>
